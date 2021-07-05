5a76ce78375838c21ed3097ce7d5fc2df7e4c530
reza seddighi <r@r.com>
2021-07-05 08:20:17 +0430

    adding typography

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import classes from "./logo.module.css";
 
 export default function Logo() {
   return (
     <span className={classes.logo}>
-      <Image width={89} height={89} alt="LUXITEC Logo" />
+      <Image
+        src={"/images/sites/logo.png"}
+        width={89}
+        height={89}
+        alt="LUXITEC Logo"
+      />
     </span>
   );
 }
--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./navigation.module.css";
 
+import Logo from "./logo";
+
 export default function Navigation() {
   return (
     <header className={classes.header}>
